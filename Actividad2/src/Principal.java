5a91141e2682c6a514b961ef1edc502ad5390413
Prissyplanet <xxelferxx14@outlook.com>
2021-03-09 21:22:07 -0700

    Initial commit

---
+++ Actividad2/src/Principal.java
@@ -0,0 +1,16 @@
+public class Principal {
+    public static void main(String[]args){
+        short x1=1;
+        int x2=6;
+        short x3=12;
+        boolean entro=false;
+        String txt="Hola mundo";
+
+        int num1=10;
+        int num2=6;
+        int res=num2++*2;
+        System.out.println(++num2);
+        System.out.println(num2);
+
+    }
+}
---
+++ HolaMundo/src/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    static int edad=18;
+    int y=0;
+    public static void main(String[] args){
+        System.out.println("Has vivido "+ (edad*365) + " días");
+    }
+}
