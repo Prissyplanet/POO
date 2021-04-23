c077562c41e2202b6659857f438d916248e43d5f
Prissyplanet <xxelferxx14@outlook.com>
2021-04-23 15:35:47 -0600

    planetas ya terminados

---
+++ Planetas/src/Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+    public static void main(String[]args){
+        Planet x= new Planet("Tierra",1500,"solida",365, 50000);
+        Planet y= new Planet("Jupiter",1500,"gaseosa",4329, 500000000);
+        Planet z= new Planet("Saturno",1500,"gaseosa",10585, 900000);
+
+        Asteroid a= new Asteroid("KPDP","solida",50000,299);
+        Asteroid b= new Asteroid("AFPA","solida",890000,1010100);
+        Asteroid c= new Asteroid("TAAP","solida",234567,87548);
+
+    }
+}
