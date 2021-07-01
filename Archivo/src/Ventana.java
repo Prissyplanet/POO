b5c53f2a7fdc2e372eb25ce88f5337899649e19e
Prissyplanet <xxelferxx14@outlook.com>
2021-06-30 21:20:07 -0600

    Archivo

--- Archivo/src/Ventana.java
+++ Archivo/src/Ventana.java
@@ -2,7 +2,9 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -15,6 +17,8 @@ import java.io.*;
 public class Ventana {
     private TextArea textArea;
     private Stage stage;
+    private ListView lv1;
+    String[] renglones;
     public Ventana(Stage stage){this.stage=stage;}
     public AnchorPane crearVentana(){
         AnchorPane pane= new AnchorPane();
@@ -41,10 +45,40 @@ public class Ventana {
         });
         textArea=new TextArea();
         textArea.setPrefSize(200,200);
-        textArea.setLayoutX(10);
-        textArea.setLayoutY(50);
-        leerArchivo("./src/datos.txt");
-        pane.getChildren().addAll(textArea,menuBar);
+        textArea.setLayoutX(10); textArea.setLayoutY(50);
+
+        lv1=new ListView(); lv1.setPrefSize(230,400); lv1.setLayoutX(10); lv1.setLayoutY(90);
+        TextField textField=new TextField(); textField.setPrefSize(150, 20); textField.setLayoutX(10); textField.setLayoutY(50); textField.setPromptText("Elemento a buscar");
+        Button btnBuscar=new Button("Buscar"); btnBuscar.setPrefSize(80,20); btnBuscar.setLayoutX(165); btnBuscar.setLayoutY(50);
+        btnBuscar.setStyle("-fx-background-color:green; -fx-background-radius:5em;-fx-text-fill:white");
+
+        Label simboloElemento=new Label("H"); simboloElemento.setFont(new Font("Arial",150)); simboloElemento.setStyle("-fx-text-fill:#111");
+        simboloElemento.setLayoutX(450); simboloElemento.setLayoutY(80);
+
+        Label nombreElemento=new Label("Hidrógeno"); nombreElemento.setFont(new Font("Arial",30)); nombreElemento.setStyle("-fx-text-fill:#0489d1");
+        nombreElemento.setLayoutX(430); nombreElemento.setLayoutY(250);
+
+        ListView listaPropiedades=new ListView(); listaPropiedades.setLayoutX(430); listaPropiedades.setLayoutY(300); listaPropiedades.setPrefSize(200,200);
+        pane.getChildren().addAll(lv1,textField,menuBar,btnBuscar,simboloElemento,nombreElemento, listaPropiedades);
+        leerArchivo("./src/elementos.csv");
+        //Eventos
+        lv1.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(lv1.getSelectionModel().getSelectedIndex()>-1){
+                    int index=lv1.getSelectionModel().getSelectedIndex();
+                    System.out.println(index+2);
+                    String[] elementos=renglones[index+2].split(",");
+                    simboloElemento.setText(elementos[2]);
+                    nombreElemento.setText(elementos[1]);
+                    listaPropiedades.getItems().clear();
+                    String[] arrayPropiedades=renglones[1].split(",");
+                    for(int x=3;x<arrayPropiedades.length;x++){
+                        listaPropiedades.getItems().add(arrayPropiedades[x]+": "+elementos[x]);
+                    }
+                }
+            }
+        });
         return pane;
     }
     public void leerArchivo(String ruta){
@@ -56,7 +90,12 @@ public class Ventana {
             while( (renglon=br.readLine() ) !=null){
                     todo= todo+"\n"+renglon;
             }
-            textArea.setText(todo);
+            renglones=todo.split("\n");
+            for(int x=2; x<renglones.length; x++){
+                String[] arrayRenglon=renglones[x].split(",");
+                lv1.getItems().add(arrayRenglon[0]+"-"+arrayRenglon[1]);
+            }
+           // textArea.setText(todo);
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
             Alert alert=new Alert(Alert.AlertType.ERROR);
